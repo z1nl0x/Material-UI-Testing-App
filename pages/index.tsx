9fe5b89a613387284d6e5cdf2e1407258f11026e
z1nl0x <paulo.kreft@gmail.com>
2022-12-18 17:03:09 -0300

    Studying material buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import MuiTypography from "../components/MuiTypography";
+import MuiButton from "../components/MuiButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <MuiTypography />
+        {/* <MuiTypography /> */}
+        <MuiButton />
       </main>
     </>
   );
